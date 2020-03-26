b7673a3c694155fc91666ae217d1f05c08609e13
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2020-03-26 11:21:58 +0100

    mise à jour des routes
    securité, blocage d'accès et css

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cookieSession({
 
 // parse form arguments in POST requests
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine('html', mustache());
@@ -22,12 +23,15 @@ app.set('views', './views');
 
 /**** Liens des images ****/
 var ressources = app.use(express.static(__dirname + '/res'));
+/**** Lien du css ****/
+var css = app.use(express.static(__dirname + '/css'));
 
 /**** Routes pour voir les pages du site ****/
 
 /* Retourne sur la page d'Acceuil */
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', is_authenticated, (req, res) => {
+  if(res.locals.authenticated) { res.redirect('/dashboard'); }
+  else { res.render('index'); }
 });
 
 /* About page */
@@ -36,28 +40,40 @@ app.get('/about', (req, res) => {
 });
 
 /* page de connection */
-app.get('/sign-in', (req, res) => {
-  res.render('sign-in');
+app.get('/sign-in', is_authenticated, (req, res) => {
+  if(res.locals.authenticated) { res.redirect('/dashboard'); }
+  else { res.render('sign-in'); }
 });
 
 app.post('/sign-in', (req, res) => {
-  var id = model.login(req.body.name, req.body.password);
+  var id = model.login(req.body.email, req.body.password);
   if (id == -1) { res.redirect('/sign-in'); }
   req.session.user = id;
   res.redirect('/dashboard');
 });
 
 /* page d'inscription */
-app.get('/sign-up', (req, res) => {
-  res.render('sign-up');
+app.get('/sign-up', is_authenticated, (req, res) => {
+  if(res.locals.authenticated) { res.redirect('/dashboard'); }
+  else { res.render('sign-up'); }
 });
 
 app.post('/sign-up', (req, res) => {
-  var id = model.new_user(req.body.name, req.body.password);
-  if (id == -1) { res.redirect('new_user'); }
+  var id = model.new_user(req.body.name, req.body.email, req.body.password);
+  if (id == -1) { res.redirect('/sign-up'); }
   req.session.user = id;
-  res.redirect('/');
-})
+  res.redirect('/dashboard');
+});
+
+/* dashboard */
+app.get('/dashboard', is_authenticated, (req, res) => {
+  if(res.locals.authenticated) { 
+    var user = model.fetchUserInformations(req.session.user);
+    console.log(user);
+    res.render('dashboard'); 
+  }
+  else { res.redirect('/'); }
+});
 
 app.listen(3000, () => console.log('listening on http://localhost:3000'));
 

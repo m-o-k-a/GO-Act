d11b1f3a062bae8f29d1471c50cbb439a181db17
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2020-04-02 09:59:13 +0200

    Ajout de route envoyer un mlessage et acceder aux messages

--- server.js
+++ server.js
@@ -68,11 +68,32 @@ app.post('/sign-up', (req, res) => {
 /* dashboard */
 app.get('/dashboard', is_authenticated, (req, res) => {
   if(res.locals.authenticated) { 
+    var messagesList = model.getMessages();
     var user = model.fetchUserInformations(req.session.user);
-    res.render('dashboard', {name: user.name, lvl: user.lvl, fanlvl: user.lvl, hr: user.heartReceived, bhr: user.brokenHeartReceived, hg: user.heartGiven, bhg: user.brokenHeartGiven}); 
+    res.render('dashboard', {id: req.session.user, messages: messagesList, name: user.name, lvl: user.lvl, fanlvl: user.lvl, hr: user.heartReceived, bhr: user.brokenHeartReceived, hg: user.heartGiven, bhg: user.brokenHeartGiven}); 
   }
   else { res.redirect('/'); }
 });
+
+/* Envoi de message */
+app.post('/send-message', (req, res) => {
+  if (id == -1) { res.redirect('/sign-up'); }
+  var id = model.new_message(req.session.user, req.body.message_content, req.body.message_category.toLowerCase());
+  res.redirect('/dashboard');
+});
+
+/* Page de message */
+app.get('/message/:id', is_authenticated, (req, res) => {
+  //ENCORE LE PROBLEME QUI DECIDE DE AUSSI FAIRE LA REQUETE SUR LES CSS MAIS JE NE SAIS PAS POURQUOI IL FAIT CA UNIQUEMENT ICI
+  if(res.locals.authenticated && model.getMessage(req.params.id) != -1 && req.params.id != "animations.css"  && req.params.id != "stylesheet.css"  && req.params.id != "colors.css") { 
+    var message = model.getMessage(req.params.id);
+    var user = model.fetchUserInformations(req.session.user);
+    res.render('message', {id: req.session.user, messageInfo: message, name: user.name, lvl: user.lvl, fanlvl: user.lvl, hr: user.heartReceived, bhr: user.brokenHeartReceived, hg: user.heartGiven, bhg: user.brokenHeartGiven}); 
+  }
+  else { res.redirect('/'); }
+});
+
+/* Gestion de coeurs */
 
 /* déconnexion */
 app.get('/logout', is_authenticated_force, (req, res) => {
